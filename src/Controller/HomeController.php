54ce09b5e5e4c532435e8248c904a22b94536c26
Germe Marc Antoine <markus0059@hotmail.fr>
2022-11-23 10:00:51 +0100

    modified function index in HomeController, add function in repository category and modified view index home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryItemRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,13 +11,15 @@ use App\Repository\ProductRepository;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(ProductRepository $productRepository): Response
+    public function index(ProductRepository $productRepository, CategoryItemRepository $categoryRepository): Response
     {
         $products = $productRepository->selectlast(3);
+        $categories = $categoryRepository->selectAllInCarousel();
         return $this->render(
             'home/index.html.twig',
             [
-                'products' => $products
+                'products' => $products,
+                'categories' => $categories
             ]
         );
     }
--- src/Repository/CategoryItemRepository.php
+++ src/Repository/CategoryItemRepository.php
@@ -39,28 +39,37 @@ class CategoryItemRepository extends ServiceEntityRepository
         }
     }
 
-//    /**
-//     * @return CategoryItem[] Returns an array of CategoryItem objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('c.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    // Select all categories in carousel from home page
+    public function selectAllInCarousel(): array
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.inCarousel = true')
+            ->getQuery()
+            ->getResult();
+    }
 
-//    public function findOneBySomeField($value): ?CategoryItem
-//    {
-//        return $this->createQueryBuilder('c')
-//            ->andWhere('c.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    /**
+    //     * @return CategoryItem[] Returns an array of CategoryItem objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('c')
+    //            ->andWhere('c.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('c.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?CategoryItem
+    //    {
+    //        return $this->createQueryBuilder('c')
+    //            ->andWhere('c.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
