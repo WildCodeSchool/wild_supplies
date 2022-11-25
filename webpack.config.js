25e68cd8b5269d61b5b2f6dabf54372a8ecf568f
Natalia <nataliya_nedbaylo@yahoo.com>
2022-11-25 14:50:14 +0100

    styles build changed;

--- webpack.config.js
+++ webpack.config.js
@@ -77,20 +77,20 @@ Encore
         config.useBuiltIns = "usage";
         config.corejs = 3;
     })
-// uncomment if you use TypeScript
-// .enableTypeScriptLoader()
+    // uncomment if you use TypeScript
+    // .enableTypeScriptLoader()
 
-// uncomment if you use React
-// .enableReactPreset()
+    // uncomment if you use React
+    // .enableReactPreset()
 
-// uncomment to get integrity="..." attributes on your script & link tags
-// requires WebpackEncoreBundle 1.4 or higher
-// .enableIntegrityHashes(Encore.isProduction())
+    // uncomment to get integrity="..." attributes on your script & link tags
+    // requires WebpackEncoreBundle 1.4 or higher
+    // .enableIntegrityHashes(Encore.isProduction())
 
-// uncomment if you're having problems with a jQuery plugin
-// .autoProvidejQuery()
+    // uncomment if you're having problems with a jQuery plugin
+    // .autoProvidejQuery()
 
-// enables Sass/SCSS support
+    // enables Sass/SCSS support
     .enableSassLoader();
 const fullConfig = Encore.getWebpackConfig();
 fullConfig.devServer = {
