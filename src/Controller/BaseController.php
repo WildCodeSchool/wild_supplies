31a610bb19f28b3eecc4c41cea2eba7c93f216f5
Natalia <nataliya_nedbaylo@yahoo.com>
2022-11-28 20:41:19 +0100

    code quality bugs resolved;

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -6,7 +6,7 @@ use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
- * @method User getUser(): array
+ * @method User getUser()
  */
 abstract class BaseController extends AbstractController
 {
