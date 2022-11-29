8eb05b7f9c620b390e2efee9ccc3b9839bcc6e18
Germe Marc Antoine <markus0059@hotmail.fr>
2022-11-29 14:56:50 +0100

    add password security with fixture entities and ADMIN

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -48,7 +48,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('app_index'));
+        return new RedirectResponse($this->urlGenerator->generate('app_home'));
         //throw new \Exception('TODO: provide a valid redirect inside ' . __FILE__);
     }
 
